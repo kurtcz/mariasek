e51aef7f098a478cf10a307e55ba63c3e63915e2
Kurt <daseeka@email.cz>
2018-01-21 21:07:00 +0100

    Special risk factor used in one rule when defending a hundred

--- Mariasek.SharedClient/GameComponents/ToggleButton.cs
+++ Mariasek.SharedClient/GameComponents/ToggleButton.cs
@@ -6,33 +6,58 @@ namespace Mariasek.SharedClient.GameComponents
 {
     public class ToggleButton : Button
     {
+        private Color _origBackgroundColor;
+        private Color _origTextColor;
+        private Color _origBorderColor;
         private bool _isSelected;
         public bool IsSelected
         { 
             get { return _isSelected; }
             set
             {
-                _isSelected = value;
-                TextColor = _isSelected ? Color.Yellow : Color.White;
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    if (_isSelected)
+                    {
+                        _origBackgroundColor = BackgroundColor;
+                        _origTextColor = TextColor;
+                        _origBorderColor = BorderColor;
+                        if (_origBackgroundColor != Color.Transparent)
+                        {
+                            BackgroundColor = Color.DarkGreen;
+                            TextColor = Color.White;
+                        }
+                        else
+                        {
+                            TextColor = Color.Yellow;
+                        }
+                        if (_origBorderColor != Color.Transparent)
+                        {
+                            BorderColor = Color.White;
+                        }
+                        ClickSound = Game.OnSound;
+                    }
+                    else
+                    {
+                        BackgroundColor = _origBackgroundColor;
+                        TextColor = _origTextColor;
+                        BorderColor = _origBorderColor;
+                        ClickSound = Game.OffSound;
+                    }
+                }
             }
         }
 
         public ToggleButton(GameComponent parent)
             : base(parent)
         {
+            _origBackgroundColor = BackgroundColor;
         }
             
         protected override void OnTouchDown(TouchLocation tl)
         {
             IsSelected = !IsSelected;
-            if (IsSelected)
-            {
-                ClickSound = Game.OnSound;
-            }
-            else
-            {
-                ClickSound = Game.OffSound;
-            }
 
             base.OnTouchDown(tl);
         }
