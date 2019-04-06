c44e6eb256a3bbbbbe081336a6ec5ad4e2186a2e
Kurt <daseeka@email.cz>
2019-04-06 20:08:49 +0200

    Bugfix in ai strategy

--- Mariasek.Engine.New/AiStrategy2.cs
+++ Mariasek.Engine.New/AiStrategy2.cs
@@ -2844,14 +2844,16 @@ namespace Mariasek.Engine.New
                                                                  .Select(i => i.Key)
                                                                  .First();
 
-                        return ValidCards(c1, c2, hands[MyIndex]).Where(i => i.Suit == preferredSuit)
-                                                             .OrderBy(i => i.Value)
-                                                             .FirstOrDefault();
+                        return ValidCards(c1, c2, hands[MyIndex]).Where(i => i.Suit == preferredSuit &&
+                                                                             i.Value != Hodnota.Eso &&
+                                                                             i.Value != Hodnota.Desitka)
+                                                                 .OrderBy(i => i.Value)
+                                                                 .FirstOrDefault();
                     }
                     return ValidCards(c1, c2, hands[MyIndex]).Where(i => i.Value != Hodnota.Eso &&
-                                                                     i.Value != Hodnota.Desitka)
-                                                         .OrderBy(i => i.Value)
-                                                         .FirstOrDefault();
+                                                                         i.Value != Hodnota.Desitka)
+                                                             .OrderBy(i => i.Value)
+                                                             .FirstOrDefault();
                 }
             }; 
 
